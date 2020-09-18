5c33f52bc8c1685ed4d07b54530b455d275e48cf
Tran Hoang <hoangtranquang@admicro.vn>
2020-09-18 18:16:13 +0700

    Add read input with cosole prob1

--- src/main/java/stc/prob1/Sol.java
+++ src/main/java/stc/prob1/Sol.java
@@ -8,24 +8,29 @@ import java.util.*;
  **/
 public class Sol {
     /**
-     *
      * Ý tưởng: tìm số lớn nhất và số lớn thứ 2 trong tất cả các mảng con có 5 phần tử
      * - Nếu phần tử đang xét = số lớn thứ nhất và lớn hơn số thứ 2 thì lấy số lớn nhất trừ đi số lớn thứ 2 là ra số hộ gia đình
-     *
-     * */
-    public List<Integer> buildings = new ArrayList<>();
-
+     */
     public static void main(String[] args) {
         Sol sol = new Sol();
-        sol.buildings = Arrays.asList(0, 0, 225, 214, 82, 73, 241, 233, 179, 219, 135, 62, 36, 13, 6, 71, 179, 77, 67, 139, 31, 90, 9, 37, 93, 203, 150, 69, 19, 137, 28, 174, 32, 80, 64, 54, 18, 0, 158, 73, 173, 20, 0, 102, 107, 48, 50, 161, 246, 145, 225, 31, 0, 153, 185, 157, 44, 126, 153, 233, 0, 201, 83, 103, 191, 0, 45, 0, 131, 87, 97, 105, 97, 209, 157, 22, 0, 29, 132, 74, 2, 232, 44, 203, 0, 51, 0, 244, 212, 212, 89, 53, 50, 244, 207, 144, 72, 143, 0, 0);
-        sol.run(1);
+        Scanner input = new Scanner(System.in);
+        for (int testCase = 1; testCase <= 10; testCase++) {
+            int subTest = input.nextInt();
+
+            List<Integer> buildings = new ArrayList<>();
+
+            for (int index = 0; index < subTest; index++) {
+                int value = input.nextInt();
+                buildings.add(value);
+            }
+
+            long result = sol.count(buildings);
+
+            System.out.printf("#%d %d\n", testCase, result);
+        }
     }
 
-    public void run(int testCase) {
-        System.out.printf("#%d %d\n", testCase, count());
-    }
-
-    public int count() {
+    public long count(List<Integer> buildings) {
         // Trường hợp đặc biệt
         if (buildings.size() < 5) {
             buildings.sort(new Comparator<Integer>() {
@@ -40,7 +45,7 @@ public class Sol {
             } else return buildings.get(0);
         }
 
-        int totalHouseholds = 0;
+        long totalHouseholds = 0;
         for (int i = 0; i < buildings.size() - 2; i++) {
 
             // tính toán lấy các toà nhà trong phạm vi xét
--- src/test/java/stc/prob1/TestSolution.java
+++ src/test/java/stc/prob1/TestSolution.java
@@ -1,55 +1,55 @@
-package stc.prob1;
-
-import stc.utils.FileHelper;
-import stc.utils.FuncHelper;
-import org.junit.Test;
-
-import java.util.List;
-
-
-/**
- * @author : hoangtq
- * @since : 22:40 13/09/2020, Sun
- **/
-public class TestSolution {
-
-    @Test
-    public void run() {
-        int[] arr = new int[]{0, 0, 225, 214, 82, 73, 241, 233, 179, 219, 135, 62, 36, 13, 6, 71, 179, 77, 67, 139, 31, 90, 9, 37, 93, 203, 150, 69, 19, 137, 28, 174, 32, 80, 64, 54, 18, 0, 158, 73, 173, 20, 0, 102, 107, 48, 50, 161, 246, 145, 225, 31, 0, 153, 185, 157, 44, 126, 153, 233, 0, 201, 83, 103, 191, 0, 45, 0, 131, 87, 97, 105, 97, 209, 157, 22, 0, 29, 132, 74, 2, 232, 44, 203, 0, 51, 0, 244, 212, 212, 89, 53, 50, 244, 207, 144, 72, 143, 0, 0};
-        Sol sol = new Sol();
-
-        for (int i = 0; i < arr.length; i++) {
-            sol.buildings.add(arr[i]);
-        }
-
-        sol.run(1);
-    }
-
-    @Test
-    public void runFile() {
-        Sol sol = new Sol();
-
-        List<String> testCases = FileHelper.readLines("inputs/problem1.txt");
-        for (int index = 0; index < testCases.size(); index += 2) {
-
-            int testcase = index / 2 + 1;
-            if (index % 2 == 1) {
-                testcase += 1;
-            }
-
-            int totalBuilding = Integer.parseInt(testCases.get(index));
-            List<Integer> buildings = FuncHelper.stringToIntegers(testCases.get(index + 1));
-
-            if (totalBuilding != buildings.size()) {
-                System.out.printf("Test %d thiếu: \n", testcase);
-                System.out.printf("Total buildings =  %d, length = %d\n", totalBuilding, buildings.size());
-                continue;
-            }
-
-            sol.buildings = buildings;
-
-            sol.run(testcase);
-        }
-
-    }
-}
+//package stc.prob1;
+//
+//import stc.utils.FileHelper;
+//import stc.utils.FuncHelper;
+//import org.junit.Test;
+//
+//import java.util.List;
+//
+//
+///**
+// * @author : hoangtq
+// * @since : 22:40 13/09/2020, Sun
+// **/
+//public class TestSolution {
+//
+//    @Test
+//    public void run() {
+//        int[] arr = new int[]{0, 0, 225, 214, 82, 73, 241, 233, 179, 219, 135, 62, 36, 13, 6, 71, 179, 77, 67, 139, 31, 90, 9, 37, 93, 203, 150, 69, 19, 137, 28, 174, 32, 80, 64, 54, 18, 0, 158, 73, 173, 20, 0, 102, 107, 48, 50, 161, 246, 145, 225, 31, 0, 153, 185, 157, 44, 126, 153, 233, 0, 201, 83, 103, 191, 0, 45, 0, 131, 87, 97, 105, 97, 209, 157, 22, 0, 29, 132, 74, 2, 232, 44, 203, 0, 51, 0, 244, 212, 212, 89, 53, 50, 244, 207, 144, 72, 143, 0, 0};
+//        Sol sol = new Sol();
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            sol.buildings.add(arr[i]);
+//        }
+//
+//        sol.run(1);
+//    }
+//
+//    @Test
+//    public void runFile() {
+//        Sol sol = new Sol();
+//
+//        List<String> testCases = FileHelper.readLines("inputs/problem1.txt");
+//        for (int index = 0; index < testCases.size(); index += 2) {
+//
+//            int testcase = index / 2 + 1;
+//            if (index % 2 == 1) {
+//                testcase += 1;
+//            }
+//
+//            int totalBuilding = Integer.parseInt(testCases.get(index));
+//            List<Integer> buildings = FuncHelper.stringToIntegers(testCases.get(index + 1));
+//
+//            if (totalBuilding != buildings.size()) {
+//                System.out.printf("Test %d thiếu: \n", testcase);
+//                System.out.printf("Total buildings =  %d, length = %d\n", totalBuilding, buildings.size());
+//                continue;
+//            }
+//
+//            sol.buildings = buildings;
+//
+//            sol.run(testcase);
+//        }
+//
+//    }
+//}
